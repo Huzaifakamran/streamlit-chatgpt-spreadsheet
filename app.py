c9767af4eb9e2823f8e57713c00856b5bb39a6fd
Muhammad Huzaifa <huzaifa.2014@hotmail.com>
2024-11-26 03:09:58 +0500

    resolve fix(#1)

--- app.py
+++ app.py
@@ -20,7 +20,20 @@ def chatGPT(user_query, data):
 
 # Google Sheets fetch data function
 def fetch_data():
-    creds_dict = st.secrets["gcp_service_account"]
+    creds_dict = {
+  "type": st.secrets["type"],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key": st.secrets["private_key"],
+  "client_email":st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url":st.secrets["client_x509_cert_url"],
+  "universe_domain": st.secrets["universe_domain"]
+}
+
     creds_json = json.dumps(creds_dict)
     # Google Sheets setup
     scope = [
